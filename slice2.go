98e4b730089eeb419f5b4f4faae0a9f93c5ff6c5
Ichiran Akita <ichiranakita@gmail.com>
2017-10-15 18:03:58 +0800

    sequencer programming

---
+++ slice2.go
@@ -0,0 +1,11 @@
+// slice2.go
+package main
+
+import "fmt"
+
+func main() {
+  mySlice := make([]int, 5, 10)
+
+  fmt.Println("len(mySlice):", len(mySlice))
+  fmt.Println("cap(mySlice):", cap(mySlice))
+}
